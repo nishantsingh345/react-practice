4e925aeb34a6d6f9b61fecab48b61074ece458e1
Nishant <nishantsingh95340@gmail.com>
2023-02-21 22:08:55 +0530

    add conditional

--- src/components/Conditinoal.js
+++ src/components/Conditinoal.js
@@ -1,8 +1,16 @@
 import React, { useState } from "react";
 
 const Conditinoal = () => {
-  const [login, setLogin] = useState(false);
-  return <div>{login ? <h1>Conditinoal true</h1> : <h1>Nishant singh</h1>}</div>;
+  const [login, setLogin] = useState();
+
+  return (
+    <div>
+      <h1>Nishant singh</h1>
+    </div>
+  );
+
+  // const [login, setLogin] = useState(false);
+  // return <div>{login ? <h1>Conditinoal true</h1> : <h1>Nishant singh</h1>}</div>;
 };
 
 export default Conditinoal;
