ede9ccbe9664fac1e0ca9275d66fd4eeea76487e
Nishant <nishantsingh95340@gmail.com>
2023-02-24 23:28:44 +0530

    initial commit

--- src/components/Conditinoal.js
+++ src/components/Conditinoal.js
@@ -1,13 +1,9 @@
 import React, { useState } from "react";
 
 const Conditinoal = () => {
-  const [login, setLogin] = useState();
+  const [login, setLogin] = useState(false);
 
-  return (
-    <div>
-      <h1>Nishant singh</h1>
-    </div>
-  );
+  return <div>{login ? <h1>Nishant singh</h1> : <h1>Bharat singh</h1>}</div>;
 
   // const [login, setLogin] = useState(false);
   // return <div>{login ? <h1>Conditinoal true</h1> : <h1>Nishant singh</h1>}</div>;
